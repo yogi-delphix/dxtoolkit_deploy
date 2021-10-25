0720b3edcfa28b40be2ce66cfa778ad9265afef2
yogi-delphix <88733926+yogi-delphix@users.noreply.github.com>
2021-10-25 13:17:09 +0530

    Update push_dxtoolkit.sh
    Updated toolkit version

--- push_dxtoolkit.sh
+++ push_dxtoolkit.sh
@@ -5,7 +5,7 @@ sed -e "s/10.0.X.10/10.0.${1}.10/g" -i dxtools.conf.${1}
 echo ""
 echo "Sending dxtoolkit tar to 10.0.${1}.30"
 echo ""
-scp dxtoolkit2-develop-redhat6-installer.tar.gz dxtools.conf.${1} install_dxtoolkit.sh delphix@10.0.${1}.30:/home/delphix/
+scp dxtoolkit2-v2.4.13-redhat6-installer.tar.gz dxtools.conf.${1} install_dxtoolkit.sh delphix@10.0.${1}.30:/home/delphix/
 echo ""
 echo "Installing dxtoolkit"
 ssh delphix@10.0.${1}.30 "/home/delphix/install_dxtoolkit.sh ${1}"
